42348be550f848e564834c1df77852dfe00efe50
rkirton <rpk218@lehigh.edu>
2016-02-23 23:09:16 +0000

    updatedhw

---
+++ hw04/ToHex.java
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////
+//Ryan Kirton
+//hw04
+//ToHex
+//
+//
+//
+//
+
+import java.util.Scanner;
+
+public class ToHex{
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        
+        System.out.println("Please enter 3 numbers representing RGB values: ");
+        int red = myScanner.nextInt();
+        int green = myScanner.nextInt();
+        int blue = myScanner.nextInt();
+        
+        
+        if(red=>0 && red
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
