049277e6b9db1af43b03626eb6ed79585982901f
rkirton <rpk218@lehigh.edu>
2016-03-21 20:24:14 +0000

    updates

---
+++ lab07/Bowtie.java
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////
+//Ryan Kirton
+//lab07
+//Bowtie
+
+import java.util.Scanner;
+
+public class Bowtie{
+    public static void main (String[] args){
+        
+        Scanner myScanner = new Scanner(System.in);
+        
+        int nStars = 9;
+        
+        for(int i=0; i<nStars; i++){
+            if(i<5){
+                for(int j=0; j<(nStars-(2*i)); j++){
+                    System.out.print("*");
+                }
+            }
+            else if(i>=5){
+                for(int j=0; j<)
+            }
+            System.out.println("");
+            
+        }
+        
+        System.out.println("");
+        
+        
+        
+    }
+}
